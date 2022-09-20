6b386a2061c3009aad1984952ac38d916ae44c2b
Colby Medeiros <medeiroscolby@gmail.com>
2022-09-20 10:13:15 -0700

    initial commit

--- cst438_register/src/main/java/com/cst438/Cst4380wRegistrationApplication.java
+++ cst438_register/src/main/java/com/cst438/Cst4380wRegistrationApplication.java
@@ -12,7 +12,7 @@ import com.cst438.service.GradebookServiceREST;
 public class Cst4380wRegistrationApplication  {  
 
 	public static void main(String[] args) {
-		SpringApplication.run(Cst4380wRegistrationApplication.class, args);
+		SpringApplication.run(Cst4380wRegistrationApplication.class, args); 
 	}
 	
 	
